ea0991f66c02b576e7635a6b36dafbd9fd46e92b
Дороватовский Евгений <e.dorovatovskyi@benovate.ru>
2022-10-02 02:04:38 +0600

    Changed settings to run application

--- core/apps/users/admin.py
+++ core/apps/users/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        'id',
+        'email',
+        'last_name',
+        'first_name',
+        'middle_name',
+    )
+    list_filter = ('is_active',)
+    list_display_links = ('email',)
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('-id',)
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': ('password',),
+            },
+        ),
+        (
+            'Персональная информация',
+            {
+                'fields': ('last_name', 'first_name', 'middle_name', 'email',),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2'),
+            },
+        ),
+    )
--- core/apps/users/models.py
+++ core/apps/users/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = EMAIL_FIELD
+    REQUIRED_FIELDS = []
+    username = None
+
+    email = models.EmailField(
+        verbose_name='E-mail пользователя',
+        unique=True,
+        max_length=50,
+    )
+    first_name = models.CharField(
+        max_length=50,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=50,
+        verbose_name='Фамилия',
+    )
+    middle_name = models.CharField(
+        max_length=50,
+        verbose_name='Отчество',
+        default='',
+        blank=True,
+    )
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
